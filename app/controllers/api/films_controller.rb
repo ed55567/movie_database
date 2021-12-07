8c800c08561b206a962de506f0edb7449759d0db
ed55567 <73504340+ed55567@users.noreply.github.com>
2021-12-06 20:59:07 -0500

    added data set

--- app/controllers/api/films_controller.rb
+++ app/controllers/api/films_controller.rb
@@ -1,2 +1,9 @@
 class Api::FilmsController < ApplicationController
+  def index
+   @films = HTTP.get("https://api.themoviedb.org/3/movie/550?api_key=#{Rails.application.credentials.films_api[:api_key]}").parse
+    render 'index.json.jb'
+  end  
 end
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    namespace :api do
+      get "/films" => "films#index"
+      post "/films" => "films#create"
+      get "/films/:id" => "films#show"
+      patch "/films/:id" => "films#update"
+      delete "/films/:id" => "films#destroy"
+    end
 end
